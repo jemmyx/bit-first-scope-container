ae92de95bc92d1a2f6c506cde67f1dc40b4abc72
fmeyer <fabio.meyer@gmail.com>
2021-01-22 13:25:50 +0100

    first commit : container of bit component helloWorld

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorldContainer msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldContainer from "./components/HelloWorldContainer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorldContainer,
+  },
+};
 </script>
 
 <style>
